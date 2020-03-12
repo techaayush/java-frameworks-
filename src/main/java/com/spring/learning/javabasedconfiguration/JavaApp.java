884477e9121c05c668bfe78e710df88219cb5096
aayushsharma <saayush008@gmail.com>
2020-03-12 13:27:48 +0530

    added main method

--- src/main/java/com/spring/learning/javabasedconfiguration/JavaApp.java
+++ src/main/java/com/spring/learning/javabasedconfiguration/JavaApp.java
@@ -1,5 +1,8 @@
 package com.spring.learning.javabasedconfiguration;
 
 public class JavaApp {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
+	}
 }
--- src/main/java/com/spring/learning/xmlbasedconfiguration/XmlApp.java
+++ src/main/java/com/spring/learning/xmlbasedconfiguration/XmlApp.java
@@ -1,5 +1,8 @@
 package com.spring.learning.xmlbasedconfiguration;
 
 public class XmlApp {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
+	}
 }
